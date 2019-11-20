e66da6c4cfaae75e9a78a4698f314d7bde312cc2
Felix Guenthner <felix-guenthner@web.de>
2019-11-20 17:11:38 +0100

    update dashboard

--- app/src/main/java/de/guenthner/trackingapp/MainActivity.java
+++ app/src/main/java/de/guenthner/trackingapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     boolean running = false;
 
+    private SeekBar seekbarBike;
+    private TextView bikeView;
+    private TextView carView;
+    private SeekBar seekbarCar;
+    private  SeekBar seekbarOpnv;
+    private TextView opnvView;
+    private Button saveBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,19 +38,107 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         steps = (TextView)findViewById(R.id.steps);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        seekbarBike = (SeekBar) findViewById(R.id.seekBarBike);
+        bikeView = (TextView) findViewById(R.id.bikeValue);
+
+        seekbarCar = (SeekBar) findViewById(R.id.seekBarCar);
+        carView = (TextView) findViewById(R.id.valueCar);
+
+        seekbarOpnv = (SeekBar) findViewById(R.id.seekBarOPNV);
+        opnvView = (TextView) findViewById(R.id.opnvValue);
+
+        saveBtn = (Button) findViewById(R.id.saveButton);
+
+
+        bikeView.setText("0 km");
+        carView.setText("0 km");
+        opnvView.setText("0 km");
+
+        seekbarBike.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //write custom code to on start progress
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                bikeView.setText(pval *  10 + " km");
+            }
+
+        });
+
+
+        seekbarCar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //write custom code to on start progress
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                carView.setText(pval *  2 + " km");
+            }
+
+        });
+
+
+        seekbarOpnv.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                opnvView.setText(pval * 10 + "km");
+
+
+            }
+        });
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekbarBike.setVisibility(View.INVISIBLE);
+                bikeView.setText("wert gespeichert");
+                seekbarCar.setVisibility(View.INVISIBLE);
+                carView.setText("wert gespeichert");
+                seekbarOpnv.setVisibility(View.INVISIBLE);
+                opnvView.setText("wert gespeichert");
+            }
+
+        });
+
+
+
+
+
+
     }
 
-      /*public void addPerson ( View view) {
 
-         TextView input = findViewById(R.id.input);
-
-         String inputText = input.getText().toString();
-
-
-         TextView output = findViewById(R.id.output);
-         output.setText(inputText);
-
-     }*/
 
     @Override
     protected void onResume(){
